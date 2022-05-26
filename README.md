f31ba8fe18a31fa831f8c4ea8e5f232ae69692af
x3nt1x <x3nt1x@gmail.com>
2022-05-26 12:24:38 +0300

    Exam

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 
 Your script must return the content of \<data\> section from the file [test.xml](http://upload.itcollege.ee/~aleksei/test.xml)<br>
 **NB! To achieve the required result apply .strip() method on the output**
+
+Joonas Mattisen  
+212956  
+IAAB
--- parse_xml.py
+++ parse_xml.py
@@ -1,12 +1,10 @@
 from bs4 import BeautifulSoup
 import requests
-import lxml
+
 
 def parse_xml():
   url = "http://upload.itcollege.ee/~aleksei/test.xml"
   xml_content = requests.get(url).content
-  soup = BeautifulSoup(xml_content,'xml')
-  
-  # your code here
-  
-  return result
+  soup = BeautifulSoup(xml_content, 'xml')
+
+  return soup.find('data').get_text().strip()
